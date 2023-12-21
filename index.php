fcb05a6a5b2b322d1b129b7bfb31deb430d0734d
root <root@ip-172-31-44-132.eu-west-3.compute.internal>
2023-12-21 12:32:37 +0000

    ajout

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 </header>
 <section class="banniere" id="banniere">
     <div class="contenu">
-        <h2>Bienvenue sur mon portfolio</h2>
+        <h2>BIENVENUE SUR MON PORTFOLIO</h2>
         <a href="#apropos" class="btn1">Qui suis-je ?</a>
     </div>
 </section>
@@ -81,103 +81,15 @@
             <div class="text">
                 <h3>Autres</h3>
             </div>
-      
- </div>
- <section class="temoignage" id="Compteur De Visite">
-    <div class="titre blanc">
-        <h2 class="titre-texte"><span>C</span>ompteur De Visites</h2>
-    </div>
-    <div class="contenu">
+	</div>
         <div class="box">
-            <div class="imbox">
-                <img src="./img/pp5.jpg" alt="">
+            		<div class="imbox">
+                                <a href="./Soft-skills.html"</a>
+                	<img src="img/Soft-skills.jpg" alt="">
+            	</div>
+            	<div class="text">
+                	<h3>Soft Skills</h3>
             </div>
-            <div class="text">
-				<?php
-if(file_exists('compteur_visites.txt'))
-{
-        $compteur_f = fopen('compteur_visites.txt', 'r+');
-        $compte = fgets($compteur_f);
-}
-else
-{
-        $compteur_f = fopen('compteur_visites.txt', 'a+');
-        $compte = 0;
-}
-if(!isset($_SESSION['compteur_de_visite']))
-{
-        $_SESSION['compteur_de_visite'] = 'visite';
-        $compte++;
-        fseek($compteur_f, 0);
-        fputs($compteur_f, $compte);
-}
-fclose($compteur_f);
-?>
-                <p><?php echo '<strong>'.$compte.'</strong> visites.';?></p>
-                <h3>Visiteurs</h3>
-            </div>
-            </div>
-        </div>
-    </div>
- </section>
- <section class="contact" id="contact">
-     <div class="titre noir">
-         <h2 class="titre-texte"><span>C</span>ontact</h2>
-         <p>Nous sommes à votre disposition en cas de besoin n'hésitez pas à nous contacter !</p>
-     </div>
-     <div class="contactform">
-        <center >
-	<h1>Contactez-nous !</h1>
-	<br>
-	<form action="send.php" method="POST" class="monForm">
 
-		<?php if(isset($_GET['error'])) {
-	 		?>
-	 		<div class="alert alert-danger" role="alert">
-                <?php
-                	$msg = "Veuillez remplir toutes les cases !";
-                 	echo $msg; ?>
-            </div>
-            <?php } ?>
-            <br>
-
-		  <label for="name">Nom :</label><br>
-  		  <input type="text" id="name" name="name"><br>
-
-		  <label for="email">E-mail :</label><br>
-		  <input type="email" id="email" name="email"><br>
-
-		   <label for="objet">Objet :</label><br>
-  		  <input type="text" id="objet" name="objet"><br>
-
-		  <label for="msg">Message :</label><br>
-		  <textarea id="msg" name="msg"></textarea><br>
-
-		  <input type="submit" name="send" value="Envoyer">
-
-	</form>
-	<br><br>
-	</center>
-</body>
-</html>
- <div class="copyright">
-     <p>copyright 2022 <a href="#">RT2 - Elbeuf</a> Tous droits reservés</p>
- </div>
- 
-
-
- <script type="text/javascript">
-     window.addEventListener('scroll', function(){
-         const header =document.querySelector('header');
-         header.classList.toggle("sticky", window.scrollY > 0 );
-     });
-
-     function toggleMenu(){
-         const tmenuToggle = document.querySelector('.menuToggle');
-         const navbar = document.querySelector('.navbar');
-         navbar.classList.toggle('active');
-         menuToggle.classList.toggle('active');
-     }
- </script>
 </body>
 </html>
